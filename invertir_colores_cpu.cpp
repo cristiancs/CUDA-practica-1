6a3a19e46e05e281c40baa888701e5a3c444e5b9
Cristian Navarrete <cristiannavarrete@outlook.com>
2019-04-02 16:18:02 -0300

    Hacer funcionar invertir colores.

---
+++ invertir_colores_cpu.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+
+void CambiarColores(float* r, float* g, float* b) {
+	//cout << *r << "|" << 1-*r << endl;
+	*r = 1 - *r;
+	*g = 1 - *g;
+	*b = 1 - *b;
+}
+
+
+int main(int argc, char const *argv[]) {
+	FILE * pFile;
+	int n, m;
+	float* r, *g, *b; 
+
+
+	pFile = fopen ("img.txt","r");
+	fscanf(pFile, "%d %d", &m, &n);
+
+	r = new float[n*m];
+	g = new float[n*m];
+	b = new float[n*m];
+
+	for (int i = 0; i < n*m; ++i) {
+		fscanf (pFile, "%f", &r[i]);
+	}
+
+	for (int i = 0; i < n*m; ++i) {
+		fscanf (pFile, "%f", &g[i]);
+	}
+
+	for (int i = 0; i < n*m; ++i) {
+		fscanf (pFile, "%f", &b[i]);
+	}
+
+
+
+	fclose (pFile);
+
+	for (int i = 0; i < (n*m); ++i) {
+		
+		CambiarColores(&r[i], &g[i], &b[i]);
+	}
+
+	FILE * pSalida;
+	pSalida = fopen ("img_salida.txt","w");
+	fprintf(pSalida, "%d %d\n", m, n);
+	for (int i = 0; i < n*m; ++i) {
+		if(i == n*m - 1) {
+			fprintf(pSalida, "%f", r[i]);
+		} else {
+			fprintf(pSalida, "%f ", r[i]);
+		}
+		
+	}
+	fprintf(pSalida, "\n");
+	for (int i = 0; i < n*m; ++i) {
+		if(i == n*m - 1) {
+			fprintf(pSalida, "%f", g[i]);
+		} else {
+			fprintf(pSalida, "%f ", g[i]);
+		}
+	}
+	fprintf(pSalida, "\n");
+	for (int i = 0; i < n*m; ++i) {
+		if(i == n*m - 1) {
+			fprintf(pSalida, "%f", b[i]);
+		} else {
+			fprintf(pSalida, "%f ", b[i]);
+		}
+	}
+
+	//cin.get();
+	return 0;
+}
